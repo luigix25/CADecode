130b83e2775ac4a6bcc15a79c07a3c087e9d19ec
luigi <luigix25@gmail.com>
2019-04-10 15:18:18 +0200

    Cosmetic Changes

--- decode_register.h
+++ decode_register.h
@@ -1,6 +1,6 @@
 struct decode_registers{
-	uint8_t opcode; 			//instruction pointer
-	uint16_t operand1;
-	uint16_t operand2;
-	uint8_t  destination_reg;
+	uint8_t opcode; 						//opcode
+	uint16_t operand1;						//first operand
+	uint16_t operand2;						//second operand ( can be 0 )
+	uint8_t  destination_reg;				//contains the code of the destination register
 } alu_regs;
