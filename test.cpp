b6efa4b2113fa6f75e56516900dae37fc200e2fa
Matteo Suffredini <suffredinimatteo@gmail.com>
2019-04-12 12:03:48 +0200

    Remove System.h System.cpp simulator.cpp

--- test.cpp
+++ test.cpp
@@ -6,34 +6,41 @@ fetch_registers regs;
 decode_registers alu_regs;
 global_registers global_regs;
 
+void setMessage(message* mex, string& source, string& dest, void* magic) {
+	strcpy(mex->source,(const char*)source.c_str());
+	strcpy(mex->dest,(const char*)dest.c_str());
+	mex->magic_struct = magic;
+	mex->next = NULL;
+}
+
 int main(){
 	string s = "Decode";
 	Decode* dec = new Decode(s,0);
 	message * mex = new message;
+	string fetch_s(FETCH);
+	string decode_s(DECODE);
 	event  ev;
 	vector<event*> event_list;
 	ev.time = 0;
+	string message1 = "DECODE send message to ";
 
 	cout << "Start test" << endl;
 
-	cout << "Format 0" << endl;
+	cout << "FETCH send messages to DECODE: " << endl;
 
-	cout << "\t NOP ... ";
+	cout << "\t Format 0" << endl;
+
+	cout << "\t \t NOP -> ";
 
 	regs.opcode = 00000001;
 
-	strcpy(mex->source,FETCH);
-	strcpy(mex->dest,DECODE);
-	mex->magic_struct = NULL;
-	mex->next = NULL;
+	setMessage(mex, fetch_s, decode_s, NULL);
 
 	ev.m = mex;
 
 	event_list = dec->notify(&ev);
 
-	cout << event_list[0]->m->dest << endl;
-
-
+	cout << message1 << event_list[0]->m->dest << endl;
 
 	return 0;
 }
