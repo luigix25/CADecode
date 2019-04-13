d2a9201c10fa2e8a99cd00e501c6f1e5404fdfc0
Matteo Suffredini <suffredinimatteo@gmail.com>
2019-04-13 18:36:35 +0200

    setMessage sostituita con getEventList che ritorna direttamente la lista di eventi

--- test.cpp
+++ test.cpp
@@ -6,41 +6,37 @@ fetch_registers regs;
 decode_registers alu_regs;
 global_registers global_regs;
 
-void setMessage(message* mex, string& source, string& dest, void* magic) {
+vector<event*> getEventList(Decode* dec, string& source, string& dest, void* magic) {
+	
+	message * mex = new message;
 	strcpy(mex->source,(const char*)source.c_str());
 	strcpy(mex->dest,(const char*)dest.c_str());
 	mex->magic_struct = magic;
 	mex->next = NULL;
+	event  ev;
+	ev.time = 0;
+	ev.m = mex;
+	return dec->notify(&ev);
 }
 
 int main(){
-	string s = "Decode";
-	Decode* dec = new Decode(s,0);
-	message * mex = new message;
 	string fetch_s(FETCH);
 	string decode_s(DECODE);
-	event  ev;
-	vector<event*> event_list;
-	ev.time = 0;
+	vector<event*> event_list;	
+	Decode* dec = new Decode("Decode",0);
+
 	string message1 = "DECODE send message to ";
-
 	cout << "Start test" << endl;
-
 	cout << "FETCH send messages to DECODE: " << endl;
-
 	cout << "\t Format 0" << endl;
 
+	//F0: NOP
 	cout << "\t \t NOP -> ";
-
 	regs.opcode = 00000001;
-
-	setMessage(mex, fetch_s, decode_s, NULL);
-
-	ev.m = mex;
-
-	event_list = dec->notify(&ev);
-
+	event_list = getEventList(dec,fetch_s, decode_s, NULL);
 	cout << message1 << event_list[0]->m->dest << endl;
+
+
 
 	return 0;
 }
